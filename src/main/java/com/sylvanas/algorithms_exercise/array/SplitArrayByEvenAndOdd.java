7719232fe0bb986ec1d8cf22b89c4477eccbc55d
SylvanasSun <sylvanassun_xtz@163.com>
2018-05-14 00:37:47 +0800

    Fix the bug for algorithms problem of array: SplitArrayByEvenAndOdd
     - Fixed a problem which Even order.

--- src/main/java/com/sylvanas/algorithms_exercise/array/SplitArrayByEvenAndOdd.java
+++ src/main/java/com/sylvanas/algorithms_exercise/array/SplitArrayByEvenAndOdd.java
@@ -4,29 +4,29 @@ package com.sylvanas.algorithms_exercise.array;
  * 分割一个整数数组，使得奇数在前偶数在后。
  * Example: 给定 [1, 2, 3, 4]，返回 [1, 3, 2, 4]。
  * 挑战: 在原数组中完成，不使用额外空间。
- *
+ * <p>
  * Created by SylvanasSun on 9/28/2017.
  */
 public class SplitArrayByEvenAndOdd {
 
-    /**
-     * 通过头尾指针判断是否为偶数,如果为偶数就交换到尾指针
-     */
     public static void solution(int[] arr) {
         if (arr == null || arr.length == 0)
             return;
 
-        int tail = arr.length - 1;
         int head = 0;
+        int tail = arr.length - 1;
+        int temp;
 
         while (head < tail) {
-            if (arr[head] % 2 == 0) {
-                int temp = arr[tail];
-                arr[tail] = arr[head];
-                arr[head] = temp;
-                tail--;
-            } else {
+            while (arr[head] % 2 == 1)
                 head++;
+            while (arr[tail] % 2 == 0)
+                tail--;
+
+            if (head < tail) {
+                temp = arr[head];
+                arr[head] = arr[tail];
+                arr[tail] = temp;
             }
         }
     }
